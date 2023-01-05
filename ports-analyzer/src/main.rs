89a20f72234682f634e0c90fdbbf84e6e3588f89
Ala Eddine Battikh <48839940+Aladeenb@users.noreply.github.com>
2023-01-06 01:37:33 +0200

    Adapted tokio

--- ports-analyzer/src/main.rs
+++ ports-analyzer/src/main.rs
@@ -1,20 +1,22 @@
-use std::net::{TcpStream, ToSocketAddrs};
-use std::time::Duration;
+use tokio::net::TcpStream;
 
-fn main() {
-    let timeout = Duration::from_secs(1);
-    let addr = "127.0.0.1:0".to_socket_addrs().unwrap().next().unwrap();
-    // TODO let the user decides which address to scan (in case of admin)
-    // TODO is it possible to scan sockets from different ips?
-    // TODO let the user decides which ports to scan?
-
-    // TODO add loading message
-    for port in 1..65535 {
-        match TcpStream::connect_timeout(&addr, timeout) {
-            Ok(_) => println!("Port {} is open", port),
-            Err(_) => continue, // We are not interested in printing the closed ports. hence `continue` skips to the next iter.
-        }
+async fn scan_port(host: &str, port: u16) -> Result<(), Box<dyn std::error::Error>> {
+    let addr = format!("{}:{}", host, port);
+    match TcpStream::connect(&addr).await {
+        Ok(_) => println!("Port {} is open", port),
+        Err(_) => println!("Port {} is closed", port),
     }
-    print!("Scan completed!")
-    // TODO reformat ouput
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let host = "localhost";
+    for port in 1..1024 {
+        tokio::spawn(async move {
+            scan_port(host, port).await.unwrap();
+        });
+    }
+    Ok(())
+    // TODO reformat output
 }
