4861d8235b6201a8ed17321c6f237c563159da1d
aladeenb <aladeen.btk@gmail.com>
2023-01-09 23:30:49 +0200

    added loop per connection

--- chat-server/src/main.rs
+++ chat-server/src/main.rs
@@ -10,9 +10,17 @@ async fn main() {
 
     let (mut socket, _addr) = listener.accept().await.unwrap();
 
-    let mut buffer = [0u8; 1024];
+    loop {
+        let mut buffer = [0u8; 1024];
 
-    let bytes_read = socket.read(&mut buffer).await.unwrap();
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
 
-    socket.write_all(&buffer[..bytes_read]).await.unwrap(); // .. = up to
+        socket.write_all(&buffer[..bytes_read]).await.unwrap(); // .. = up to
+    }
+
+    /*
+        for now, only one client can read and writes. Also only one message is supported.
+        The user have to close the connection and run again to send a new message.
+    */
+    // TODO make the program supports multiple messages
 }
